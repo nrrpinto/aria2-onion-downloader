ad07b8c7a522ed9928b511be53f6128c083c1a08
Nuno Pinto <npinto@asceris.com>
2024-06-17 11:40:50 +0100

    Create log files for v2ray proxy and change to have the right permissions.

--- downloader/run/run_aria2.sh
+++ downloader/run/run_aria2.sh
@@ -2,7 +2,8 @@
 set -e
 
 touch /conf/aria2.session
-touch /log/aria2_log.txt
+touch /log/aria2_log.txt /log/v2ray_access.log /log/v2ray_error.log
+chmod 644 /log/*.log
 
 python /home/creatorrc/creatorrc.py --speetor && cat tor_config.txt >> /conf/torrc && tor --runasdaemon 1 -f /conf/torrc || tor --runasdaemon 1
 
