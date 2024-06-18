8782242968f746d9950edde74b3451ab638547c5
f4d0 <nuno.r.r.pinto@gmail.com>
2024-06-18 07:36:16 +0100

    Update run_aria2.sh
    Add user agent to the execution of aria2c

--- downloader/run/run_aria2.sh
+++ downloader/run/run_aria2.sh
@@ -8,5 +8,5 @@ chmod 644 /log/*.log
 python /home/creatorrc/creatorrc.py --speetor && cat tor_config.txt >> /conf/torrc && tor --runasdaemon 1 -f /conf/torrc || tor --runasdaemon 1
 
 exec v2ray run -c /conf/config.json &
-exec aria2c --conf-path=/conf/aria2.conf --log=/log/aria2_log.txt --rpc-listen-port=${RPCPORT} --rpc-secret=${RPCSECRET}
+exec aria2c --conf-path=/conf/aria2.conf --log=/log/aria2_log.txt --rpc-listen-port=${RPCPORT} --rpc-secret=${RPCSECRET} --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15"
 
